c13c5769950e8d99de94013695cf1b70c5cc4021
moy5 <moy5@wisc.edu>
2020-02-07 18:19:38 -0600

    Did the changes

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.util.Log;
+import android.view.View;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickFunction(View view) {
+        // Log.i( "Info", "Button pressed");
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        String str = myTextField.getText().toString();
+
+        //Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+        goToActivity2(str);
+    }
+
+    public void goToActivity2(String s)
+    {
+        Intent intent = new Intent(this,  Main2Activity.class);
+        intent.putExtra("message",s);
+        startActivity(intent);
     }
 }
